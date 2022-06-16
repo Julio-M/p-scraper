3fdaaaa3f0cc78165723c20df371d24a1582d61f
Xhulio Mihali <mihalijulio@gmail.com>
2022-06-16 13:36:14 -0500

    json

--- p_scrape.py
+++ p_scrape.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import re
+import json
 
 URL = "https://en.wikipedia.org/wiki/List_of_programming_languages"
 response = requests.get(URL)
@@ -11,8 +11,17 @@ data = soup.find(id="mw-content-text")
 
 results = data.find_all('li')
 
+all_data = []
+
 for content in results:
+    data = {}
     language = content.text
+    data['language'] = language
     source = content.find('a').get('href')
     if source != None:
-        print(f'https://en.wikipedia.org{source}')
+        data['source'] = (f'https://en.wikipedia.org{source}')
+    all_data.append(data)
+
+
+with open("data.json", "w") as outfile:
+    json.dump(all_data, outfile)
